9df2ef9f76408703d3fa3f28547ae51044608a08
alinecintracamargo <alineandradecintra@gmail.com>
2024-12-17 17:47:32 -0300

    .

---
+++ course-manager/src/app/course/course-list-component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from "@angular/core";
+import { Course } from "./course";
+
+@Component({
+    selector: 'app-course-list',
+    templateUrl: './course-list-component.html'
+})
+export class CourseListComponent implements OnInit {
+
+        course: Course[] = [];
+
+        ngOnInit(): void {
+            this.course = [
+                {
+                    id: 1,
+                    name: 'Angular-Forms',
+                    imageUrl: '',
+                    price: 99.99,
+                    code: 'XPS-8796',
+                    duration: 120,
+                    rating: 4.5,
+                    releaseDate: 'november, 2 , 2024'
+
+                },
+
+                {
+                    id: 2,
+                    name: 'Angular-HTML',
+                    imageUrl: '',
+                    price: 49.99,
+                    code: 'LKL-1094',
+                    duration: 80,
+                    rating: 4,
+                    releaseDate: 'november, 2 , 2024'
+
+                }
+
+            ]
+        }
+
+}
