1a48bc164647117b3e81aa71c187e9bdd0dfb06f
alinecintracamargo <alineandradecintra@gmail.com>
2024-12-19 19:00:35 -0300

    .

--- course-manager/src/app/course/course-info.component.ts
+++ course-manager/src/app/course/course-info.component.ts
@@ -21,7 +21,10 @@ export class CourseInfoComponent implements OnInit {
     }
 
     save(): void{
-        this.courseService.save(this.course);
+        this.courseService.save(this.course).subscribe({
+            next: course => console.log('Saved with sucess', course),
+            error: err => console.log('Error', err)
+        });
     }
 
 
