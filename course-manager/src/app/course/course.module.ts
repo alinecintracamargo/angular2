550ce5aa11026dfa929dd5d709f7225fab8e9fbe
alinecintracamargo <alineandradecintra@gmail.com>
2024-12-19 20:52:02 -0300

    feat: Segregating modules

--- course-manager/src/app/course/course.module.ts
+++ course-manager/src/app/course/course.module.ts
@@ -3,22 +3,22 @@ import { CourseListComponent } from "./course-list-component";
 import { CourseInfoComponent } from "./course-info.component";
 import { RouterModule } from "@angular/router";
 import { FormsModule } from "@angular/forms";
-import { ReplacePipe } from "../pipe/replace.pipe";
 import { CommonModule } from "@angular/common";
-import { StarComponent } from "../star/star.component";
+import { StarModule } from "../shared/components/star/star.module";
+import { AppPipeModule } from "../shared/pipe/app-pipe-module";
 
 @NgModule({
     declarations: [
 
         CourseListComponent,
         CourseInfoComponent,
-        ReplacePipe,
-        StarComponent
 
     ],
     imports: [
         FormsModule,
         CommonModule,
+        StarModule,
+        AppPipeModule,
         RouterModule.forChild([
         {
             path: 'courses/info/:id', component: CourseInfoComponent
