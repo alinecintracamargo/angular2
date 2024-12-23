0054d3bae310470d9f5e7b80b73b5df0dfc43873
alinecintracamargo <alineandradecintra@gmail.com>
2024-12-23 11:40:05 -0300

    .

--- course-manager/src/app/app.module.ts
+++ course-manager/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { Error404Component } from './error-404/error-404.component';
 import { CourseModule } from './course/course.module';
 import { CoreModule } from './core/core.module';
 
@@ -12,7 +11,6 @@ import { CoreModule } from './core/core.module';
 @NgModule({
   declarations: [
     AppComponent,
-    Error404Component,
   ],
   imports: [
     BrowserModule,
@@ -24,9 +22,6 @@ import { CoreModule } from './core/core.module';
     {
         path: '', redirectTo: 'courses', pathMatch: 'full'
     },
-    {
-      path: '**', component: Error404Component
-    }
   ])
   ],
   providers: [],
